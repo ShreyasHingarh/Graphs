0fd039d852bb8a62e1edb0c9ea79a22261cf52be
GMR Student <gmrstudent@outlook.com>
2022-06-19 11:32:00 -0700

    made classes

---
+++ The Un-Graphs/The Un-Graphs/Graphs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Un_Graphs
+{
+    public class Vertex<T>
+    {
+        public T value { get; set; }
+        public List<Vertex<T>> Neighbors { get; set; }
+
+        public int NeighborCount => Neighbors.Count;
+
+        public Vertex(T value) {}
+    }
+
+    public class Graphs
+    {
+        public List<Vertex<T>> Vertices { get; private set; }
+
+        public int VertexCount => Vertices.Count;
+
+        public Graph() { }
+
+    }
+}
