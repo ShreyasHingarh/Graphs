5c78707aea3aef51fedf235a40dc011a81a29ba2
GMR Student <gmrstudent@outlook.com>
2022-06-26 10:14:12 -0700

    finished

--- The Un-Graphs/The Un-Graphs/Graphs.cs
+++ The Un-Graphs/The Un-Graphs/Graphs.cs
@@ -6,21 +6,90 @@ namespace The_Un_Graphs
 {
     public class Vertex<T>
     {
-        public T value { get; set; }
+        public T Value { get; set; }
         public List<Vertex<T>> Neighbors { get; set; }
 
-        public int NeighborCount => Neighbors.Count;
+        
 
-        public Vertex(T value) {}
+        public Vertex(T value) 
+        {
+            Neighbors = new List<Vertex<T>>();
+            Value = value;
+        }
+        public int NeighborCount => Neighbors.Count;
     }
 
-    public class Graphs
+    public class Graphs<T>
     {
         public List<Vertex<T>> Vertices { get; private set; }
 
         public int VertexCount => Vertices.Count;
+        public Graphs()
+        {
+            Vertices = new List<Vertex<T>>();
+        }
 
-        public Graph() { }
+        public void AddVertex(Vertex<T> vertex)
+        {
+            if(vertex != null && vertex.NeighborCount == 0 && !Vertices.Contains(vertex))
+            {
+                Vertices.Add(vertex);
+            }
+        }  
+        public bool RemoveVertex(Vertex<T> vertex)
+        {
+            if(Vertices.Contains(vertex))
+            {
+                vertex.Neighbors = new List<Vertex<T>>();
+                Vertices.Remove(vertex);
+                foreach (var vert in Vertices)
+                {
+                    if(vert.Neighbors.Contains(vertex))
+                    {
+                        vert.Neighbors.Remove(vertex);
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+        public bool AddEdge(Vertex<T> a, Vertex<T> b)
+        {
+            if(a != null && b != null && Vertices.Contains(a) && Vertices.Contains(b))
+            {
+                a.Neighbors.Add(b);
+                b.Neighbors.Add(a);
+                return true;
+            }
+            return false;
+        }
+        public bool RemoveEdge(Vertex<T> a, Vertex<T> b)
+        {
+            if(a != null && b != null && a.Neighbors.Contains(b) && b.Neighbors.Contains(a))
+            {
+                a.Neighbors.Remove(b);
+                b.Neighbors.Remove(a);
+                return true;
+            }
+            return false;
+        }
+        public Vertex<T> Search(T value)
+        {
+            int index = -1;
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                if(Vertices[i].Value.Equals(value))
+                {
+                    index = i;
+                    break; 
+                }
+            }
+            if (index == -1)
+            {
+                return null;
+            }
+            return Vertices[index];
+        }
 
     }
 }
--- The Un-Graphs/The Un-Graphs/Program.cs
+++ The Un-Graphs/The Un-Graphs/Program.cs
@@ -6,7 +6,22 @@ namespace The_Un_Graphs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Graphs<int> graph = new Graphs<int>();
+            Vertex<int> b = new Vertex<int>(2);
+            Vertex<int> c = new Vertex<int>(3);
+
+            Vertex<int> a = new Vertex<int>(1);
+            graph.AddVertex(a);
+            Vertex<int> b = a;
+            graph.AddVertex(b);
+            Console.WriteLine(graph.VertexCount);
+
+            graph.AddVertex(b);
+            graph.AddVertex(c);
+            graph.AddEdge(a, b);
+            graph.AddEdge(b, c);
+            graph.AddEdge(c, a);
+
         }
     }
 }
